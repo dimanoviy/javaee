b1e6cf44c15ae09e900dcec4cf29858e40696edb
dimanoviy <dimanoviy@yandex.ru>
2019-11-19 15:16:14 +0300

    Game of life beta version

--- src/main/java/edu/javaee/gameoflife/GameOfLife.java
+++ src/main/java/edu/javaee/gameoflife/GameOfLife.java
@@ -3,15 +3,56 @@ package edu.javaee.gameoflife;
 import java.io.*;
 
 public class GameOfLife {
-    final static int fieldSize = 50;
-    static byte[][] fieldToday = new byte[fieldSize][fieldSize];
-    static byte[][] fieldTomorrow = new byte[fieldSize][fieldSize];
-    private final static int LIFELASTS = 1000000;
+    final private static int FIELD_SIZE = 10_000;
+    final private static int LIFE_LASTS = 1_000;
+    final private static boolean MULTITHREADING = true;
+    final private static byte threadsNumber = 2;
+    final private static int MIN_NEIGHBOURS_TO_LIVE = 2;
+    final private static int MAX_NEIGHBOURS_TO_LIVE = 3;
+    static byte[][] fieldToday = new byte[FIELD_SIZE][FIELD_SIZE];
+    final static byte[][] fieldTomorrow = new byte[FIELD_SIZE][FIELD_SIZE];
+
+    public static void main(String[] args) {
+//        fillFieldFromFile("/resources/input.txt");
+        fillRandomlyField();
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < LIFE_LASTS; i++) {
+            if (!isThereLife()) {
+                break;
+            }
+            if (MULTITHREADING) {
+                calculateLifeTodayMultithreaded(threadsNumber);
+            } else {
+                calculateLifeToday();
+            }
+            fieldToday = fieldTomorrow;
+        }
+        System.out.println(System.currentTimeMillis() - t);
+    }
+
+    private static void calculateLifeTodayMultithreaded(byte threadsNumber) {
+        for (int k = 0; k < threadsNumber; k++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < FIELD_SIZE; i++) {
+                        for (int j = 0; j < FIELD_SIZE; j++) {
+                            if ((MIN_NEIGHBOURS_TO_LIVE <= surroundLife(i, j)) & (surroundLife(i, j) <= MAX_NEIGHBOURS_TO_LIVE)) {
+                                fieldTomorrow[i][j] = 1;
+                            } else {
+                                fieldTomorrow[i][j] = 0;
+                            }
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
 
     private static void calculateLifeToday() {
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
-                if ((2 <= surroundLife(i, j)) & (surroundLife(i, j) <= 3)) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                if ((MIN_NEIGHBOURS_TO_LIVE <= surroundLife(i, j)) & (surroundLife(i, j) <= MAX_NEIGHBOURS_TO_LIVE)) {
                     fieldTomorrow[i][j] = 1;
                 } else {
                     fieldTomorrow[i][j] = 0;
@@ -21,37 +62,36 @@ public class GameOfLife {
     }
 
     private static int surroundLife(int i, int j) {
-        int sum = fieldToday[prev(i)][prev(j)] + fieldToday[prev(i)][j] + fieldToday[prev(i)][next(j)] +
+        return fieldToday[prev(i)][prev(j)] + fieldToday[prev(i)][j] + fieldToday[prev(i)][next(j)] +
                 fieldToday[i][prev(j)] + fieldToday[i][next(j)] +
                 fieldToday[next(i)][prev(j)] + fieldToday[next(i)][j] + fieldToday[next(i)][next(j)];
-        return sum;
     }
 
     private static int prev(int i) {
         if (i == 0) {
-            return fieldSize - 1;
+            return FIELD_SIZE - 1;
         }
         return i - 1;
     }
 
     private static int next(int i) {
-        if (i == fieldSize - 1) {
+        if (i == FIELD_SIZE - 1) {
             return 0;
         }
         return i + 1;
     }
 
     private static void visualizeField() {
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
                 System.out.print(fieldToday[i][j] + " ");
             }
             System.out.println();
         }
     }
     private static boolean isThereLife() {
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
                 if (fieldToday[i][j] == 1) {
                     return true;
                 }
@@ -60,15 +100,11 @@ public class GameOfLife {
         return false;
     }
 
-    private static boolean lifeLasts(int k) {
-        return k < LIFELASTS;
-    }
-
     private static void fillFieldFromFile(String fileName) throws IOException {
         File file = new File(fileName);
         DataInputStream targetStream = new DataInputStream(new FileInputStream(file));
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
                 if (targetStream.read() == 48) {
                     fieldToday[i][j] = 0;
                 } else {
@@ -80,24 +116,11 @@ public class GameOfLife {
     }
 
     private static void fillRandomlyField() {
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
                 fieldToday[i][j] = (byte) ((byte) (Math.random() * 10) / 5);
             }
         }
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        fillFieldFromFile("/Users/dn/IdeaProjects/javaee/src/main/java/edu/javaee/gameoflife/input.txt");
-//        fillRandomlyField();
-        int k = 0;
-        long t = System.currentTimeMillis();
-        while (isThereLife() & lifeLasts(k)) {
-            calculateLifeToday();
-            fieldToday = fieldTomorrow;
-            k++;
-        }
-        System.out.println("This life lasted for " + k + "days");
-        System.out.println(System.currentTimeMillis() - t);
-    }
 }
